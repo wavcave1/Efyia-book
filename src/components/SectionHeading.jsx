f1a037bc9ebe7d00fdf593b7dac950f460f710b0
Ian Crosby <wavcavemgmt@gmail.com>
2026-03-29 01:36:01 +0000

    1

--- src/components/SectionHeading.jsx
+++
@@ -1,9 +0,0 @@
-export default function SectionHeading({ eyebrow, title, description, align = 'left' }) {
-  return (
-    <div className={`section-heading section-heading--${align}`}>
-      {eyebrow ? <p className="section-heading__eyebrow">{eyebrow}</p> : null}
-      <h2>{title}</h2>
-      {description ? <p className="section-heading__description">{description}</p> : null}
-    </div>
-  );
-}
